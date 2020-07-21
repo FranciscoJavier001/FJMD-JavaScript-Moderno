49ef70a858fd9ee212ce3ed0593e8fed341aa8a6
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-07-20 23:13:34 -0500

    Blackjack

--- 20_06_30-JavaScript/20_07_18-02-Blackjack/assets/js/juego.js
+++ 20_06_30-JavaScript/20_07_18-02-Blackjack/assets/js/juego.js
@@ -33,8 +33,25 @@ crearDeck();
 // Esta funcion me permite tomar una carta
 const pedirCarta = () => {
 
+    if (deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+
+    const carta = deck.pop()
 
     console.log(deck);
     console.log(carta); // Carta debe de ser de la baraja
-    return '2C';
+    return 'carta';
 }
+
+// pedirCarta();
+const valorCarta = (carta) => {
+
+    const valor = carta.substring(0, carta.length - 1);
+    return (isNaN(valor)) ?
+        (valor === 'A') ? 11 : 10 :
+        valor * 1;
+}
+
+const valor = valorCarta(pedirCarta());
+console.log({ valor });
