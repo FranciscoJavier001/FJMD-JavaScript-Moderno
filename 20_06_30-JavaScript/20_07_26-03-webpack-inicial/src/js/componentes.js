ff7f06a25bb131ea9762b24ad0c6a412d2c07c33
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-07-27 23:14:19 -0500

    Webpack

--- 20_06_30-JavaScript/20_07_26-03-webpack-inicial/src/js/componentes.js
+++ 20_06_30-JavaScript/20_07_26-03-webpack-inicial/src/js/componentes.js
@@ -1,5 +1,5 @@
-const saludar = (nombre) => {
-    console.log('Creando etiqueta h1');
+export const saludar = (nombre) => {
+    console.log('Creando etiqueta h1, en el HTML');
 
     const h1 = document.createElement('h1');
     h1.innerText = `Hola, ${nombre} `;
