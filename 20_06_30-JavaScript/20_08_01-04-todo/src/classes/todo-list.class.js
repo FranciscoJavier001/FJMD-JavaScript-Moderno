328e6d40f4b565a4ddabd6fa923d580ebd4e82bb
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-08-05 22:12:04 -0500

    Todos

--- 20_06_30-JavaScript/20_08_01-04-todo/src/classes/todo-list.class.js
+++ 20_06_30-JavaScript/20_08_01-04-todo/src/classes/todo-list.class.js
@@ -1,32 +1,57 @@
 
 
 
-export class TodoList{
+export class TodoList {
 
-    constructor(){
-        this.todos = [];
+    constructor() {
+        // this.todos = [];
+        this.crgarLocalStorage();
     }
 
-    nuevoTodo(todo){
+    nuevoTodo(todo) {
         this.todos.push(todo);
+        this.guardarLocalStorage();
     }
 
-    eliminarTodo(id){
+    eliminarTodo(id) {
         this.todos = this.todos.filter(todo => todo.id != id)
+        this.guardarLocalStorage();
     }
 
-    marcarCompletado(id){
-        for(const todo of this.todos){
-            
-            if(todo.id == id){
+    marcarCompletado(id) {
+        for (const todo of this.todos) {
+
+            if (todo.id == id) {
                 todo.completado = !todo.completado;
+                this.guardarLocalStorage();
                 break;
             }
         }
     }
 
-    eliminarCompletados(){
-        
+    eliminarCompletados() {
+        this.todos = this.todos.filter(todo => !todo.completado)
+        this.guardarLocalStorage();
     }
 
+    guardarLocalStorage() {
+        localStorage.setItem('todo', JSON.stringify(this.todos));
+    }
+
+    crgarLocalStorage() {
+        // if (localStorage.getItem('todo')) {// Verificar si el objeto existe
+        //     this.todos = JSON.parse(localStorage.getItem('todo'));
+
+        //     console.log('cargarLocal:', this.todos);
+        //     console.log(typeof this.todos);
+
+        // } else {
+        //     this.todos = [];
+        // }
+
+        this.todos = (localStorage.getItem('todo')) // Operador Ternario de la Funcion de arriba - Verifica si el objeto existe entonces
+            ? JSON.parse(localStorage.getItem('todo')) // Imprime lo que tienes en el localStorage
+            : []; // En caso contrario imprime un arreglo vacio
+
+    }
 }
--- 20_06_30-JavaScript/20_08_01-04-todo/src/index.js
+++ 20_06_30-JavaScript/20_08_01-04-todo/src/index.js
@@ -5,9 +5,18 @@ import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
 
-const tarea = new Todo('Aprender JavaScript!!'); //Aqui creamos una nueva tarea, en el constructor Todo y recibe un parametro que es Aprender JavaScript
-todoList.nuevoTodo(tarea); //Aqui ya me daba los metodos (osea "nuevoTodo")
+// const tarea = new Todo('Aprender JavaScript!!'); //Aqui creamos una nueva tarea, en el constructor Todo y recibe un parametro que es Aprender JavaScript
+// todoList.nuevoTodo(tarea); //Aqui ya me daba los metodos (osea "nuevoTodo")
 
-console.log(todoList); 
+// console.log(todoList); 
 
-crearTodoHtml(tarea);
+// crearTodoHtml(tarea);
+
+// localStorage.setItem('mi-key', 'ABC123');
+// sessionStorage.setItem('mi-key', 'ABC123');
+
+// setTimeout(() => {
+//     localStorage.removeItem('mi-key');
+// }, 1500)
+
+todoList.todos.forEach(crearTodoHtml); // Mandamos llamar este procedimiento para ver los Todos
