6504982b15adb74cf0366e079b9a0e076c64b039
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-08-23 15:47:32 -0500

    Promesas

--- 20_06_30-JavaScript/20_08_10-05-promesas-callbacks/src/index.js
+++ 20_06_30-JavaScript/20_08_10-05-promesas-callbacks/src/index.js
@@ -1,17 +1,33 @@
-import {buscarHeroe} from './js/callbacks';
+import {buscarHeroe as buscarHeroeCallback} from './js/callbacks';
+import {buscarHeroe} from './js/promesas';
+
 import './styles.css';
 
 const heroeId1 = 'capi'; // Este es el primer argumento, y el segundo argumento es una funcion conocida como callback
 const heroeId2 = 'spider'; 
 
-buscarHeroe(heroeId1, (err, heroe1) => { // Esto manda una funcion como argumento
-    if(err){return console.error(err); }
+// buscarHeroe(heroeId1, (err, heroe1) => { // Esto manda una funcion como argumento
+//     if(err){return console.error(err); }
 
-    buscarHeroe(heroeId2, (err, heroe2) => {
-        if(err) {return console.error(err);}
+//     buscarHeroe(heroeId2, (err, heroe2) => {
+//         if(err) {return console.error(err);}
 
-        console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision `);
-    });
-}); 
+//         console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision `);
+//     });
+// }); 
+
+// buscarHeroe(heroeId1).then(heroe1 => {
+//     // console.log(`Enviando a ${heroe.nombre} a la mision`);
+//     buscarHeroe(heroeId2).then(heroe2 => {
+//         console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision `);
+//     });
+// });
+
+Promise.all([buscarHeroe(heroeId1), buscarHeroe(heroeId2)])
+.then(([heroe1, heroe2]) => {
+    
+    console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision `);
+
+});
 
 console.log('Fin de programa');
---
+++ 20_06_30-JavaScript/20_08_10-05-promesas-callbacks/src/js/promesas.js
@@ -0,0 +1,27 @@
+const heroes = {
+    capi: { // Esto es un objeto
+        nombre: 'Capitan America',
+        poder: 'Aguantar inyecciones sin morir'
+    },
+    iron: { // Esto es un objeto
+        nombre: 'Ironman',
+        poder: 'Absurda cantidad de dinero'
+    },
+    spider: { // Esto es un objeto
+        nombre: 'Spiderman',
+        poder: 'La mejor reaccion alergica a las picaduras de arañas'
+    },
+}
+
+export const buscarHeroe = (id) => {
+
+    const heroe = heroes[id];
+
+    return new Promise( (resolve, reject) => {
+        if(heroe){
+            resolve(heroe);
+        } else {
+            reject(`No eiste un heroe con el id ${id}`);
+        }
+    });
+}
