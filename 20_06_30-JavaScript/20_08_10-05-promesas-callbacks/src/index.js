431bba8d89c97081c766c99e632c6b037e6bcb90
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-08-22 23:20:40 -0500

    Callback Hell

--- 20_06_30-JavaScript/20_08_10-05-promesas-callbacks/src/index.js
+++ 20_06_30-JavaScript/20_08_10-05-promesas-callbacks/src/index.js
@@ -1,14 +1,17 @@
 import {buscarHeroe} from './js/callbacks';
 import './styles.css';
 
-const heroeId = 'capi2'; // Este es el primer argumento, y el segundo argumento es una funcion conocida como callback
+const heroeId1 = 'capi'; // Este es el primer argumento, y el segundo argumento es una funcion conocida como callback
+const heroeId2 = 'spider'; 
 
-buscarHeroe(heroeId, (err, heroe) => { // Esto manda una funcion como argumento
-    if(err){
-        console.error(err);
-    } else {
-        console.info(heroe);
-    }
+buscarHeroe(heroeId1, (err, heroe1) => { // Esto manda una funcion como argumento
+    if(err){return console.error(err); }
+
+    buscarHeroe(heroeId2, (err, heroe2) => {
+        if(err) {return console.error(err);}
+
+        console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision `);
+    });
 }); 
-8heroeId
-eliminarHeroe
+
+console.log('Fin de programa');
