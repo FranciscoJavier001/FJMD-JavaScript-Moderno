abaafe47b7388e8bfcf61d424b824fd64366715c
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-08-28 23:41:29 -0500

    Promesas

--- 20_06_30-JavaScript/20_08_10-05-promesas-callbacks/src/index.js
+++ 20_06_30-JavaScript/20_08_10-05-promesas-callbacks/src/index.js
@@ -1,9 +1,10 @@
-import {promesaLenta, promesaMedia, promesaRapida} from './js/promesas';
+import {buscarHeroe, buscarHeroeAsync} from './js/promesas';
 
-// promesaLenta.then(console.log);
-// promesaMedia.then(console.log);
-// promesaRapida.then(console.log);
+buscarHeroe('capi2')
+.then(heroe => console.log(heroe))
+.catch(console.warn);
 
-Promise.race([promesaLenta, promesaMedia, promesaRapida])
-.then(console.log)
+
+buscarHeroeAsync('iron2')
+.then(heroe => console.log(heroe))
 .catch(console.warn);
