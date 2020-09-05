0aea862f0fffbfa057274c8542a16947ed675adc
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-09-05 17:10:10 -0500

    Peticiones

--- 20_06_30-JavaScript/20_09_03-06-http/src/index.js
+++ 20_06_30-JavaScript/20_09_03-06-http/src/index.js
@@ -1,6 +1,4 @@
 // import  { init } from "./js/chistes-page";
-import  { obtenerUsuarios }  from "./js/http-provider";
+import {init} from './js/usuarios-page';
 
-obtenerUsuarios().then(console.log);
-
-// init()
+init();
