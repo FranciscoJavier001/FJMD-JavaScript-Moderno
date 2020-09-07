3124c78daf9780c2a34502da00ea0c634fddb18c
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-09-06 20:07:38 -0500

    CRUD

--- 20_06_30-JavaScript/20_09_03-06-http/src/index.js
+++ 20_06_30-JavaScript/20_09_03-06-http/src/index.js
@@ -3,9 +3,14 @@
 
 import * as CRUD from './js/crud-provider';
 
-CRUD.getUsuario(2).then(console.log);
+// CRUD.getUsuario(2).then(console.log);
 
-CRUD.crearUsuario({
-    name: 'Francisco',
-    job: 'Developer'
+// CRUD.crearUsuario({
+//     name: 'Francisco',
+//     job: 'Developer'
+// }).then(console.log);
+
+CRUD.actualizarUsuario(1, {
+    name: 'Melissa',
+    job: 'Cook'
 }).then(console.log);
