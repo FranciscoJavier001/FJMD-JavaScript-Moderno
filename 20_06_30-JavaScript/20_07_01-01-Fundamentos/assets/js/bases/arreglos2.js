ae5afc960a7d7d3180162d192e09662ebe428595
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-10-08 23:56:34 -0500

    Cambios

--- 20_06_30-JavaScript/20_07_01-01-Fundamentos/assets/js/bases/arreglos2.js
+++ 20_06_30-JavaScript/20_07_01-01-Fundamentos/assets/js/bases/arreglos2.js
@@ -1,23 +1,23 @@
 let juegos = ['Zelda', 'Mario', 'Metroid', 'Chrono'];
-console.log('Largo:', juegos.length);
+// console.log('Largo:', juegos.length);
 
-let primero = juegos[2 - 2];
-let ultimo = juegos[juegos.length - 1]
+// let primero = juegos[2 - 2]; /** porque es el arreglo 0 */
+// let ultimo = juegos[juegos.length - 1] /** Se va al ultimo */
 
-console.log({ primero, ultimo });
+// console.log({ primero, ultimo });
 
-juegos.forEach((elemento, indice, arr) => {
-    console.log({ elemento, indice, arr });
-});
+// juegos.forEach((elemento, indice, arr) => { /** Estos son como salen, esos son los primeros 3 del arreglo, el nombre es lo de menos */
+//     console.log({ elemento, indice, arr });
+// });
 
-let nuevaLongitud = juegos.push('F-Zero');
-console.log({ nuevaLongitud, juegos });
+// let nuevaLongitud = juegos.push('F-Zero'); /** Agrega un nuevo juego y me muestra el tamaño del nuevo arreglo, luego me da el valor de cada array*/
+// console.log({ nuevaLongitud, juegos });
 
-nuevaLongitud = juegos.unshift('Fire Emblem');
-console.log({ nuevaLongitud, juegos });
+// nuevaLongitud = juegos.unshift('Fire Emblem'); /** Se agregan al principio */
+// console.log({ nuevaLongitud, juegos }); /** Lo que migue y los juevos ordenados por numero */
 
-let juegoBorrado = juegos.pop()
-console.log({ juegoBorrado, juegos });
+// let juegoBorrado = juegos.pop()/** Borro el ultimo */
+// console.log({ juegoBorrado, juegos }); 
 
 let pos = 1;
 
@@ -25,7 +25,7 @@ console.log(juegos)
 let juegosBorrados = juegos.splice(pos, 2);
 console.log({ juegosBorrados, juegos });
 
-let metroidIndex = juegos.indexOf('Metroid'); //CaseSensitive
-console.log(metroidIndex);
+// let metroidIndex = juegos.indexOf('Metroid'); //CaseSensitive
+// console.log(metroidIndex);
 
 // TODO: Referencia
